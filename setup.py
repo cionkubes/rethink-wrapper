3258c51cf434d3f587bbde1459744a0eadb0af7d
Erlend Tobiassen (STUDET) <erlentob@stud.ntnu.no>
2017-11-09 14:34:01 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name="async_rethink",
+    version="0.1",
+    license="GPLv3",
+    install_requires=[
+        'Rx==1.6.0',
+        'rethinkdb>=1.14',
+        'logzero==1.3.1'
+    ],
+    packages=find_packages(),
+)
